7de1cb9592175079c8cfe8d2518cdb7072646357
Ryan <ryan@burgettweb.net>
2015-11-23 23:17:54 -0500

    Configured file uploading

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-var express = require('express');
+var express = require('express'),
+    multipart = require('connect-multiparty'),
+    path = require('path');
 
 var app = express();
+
+app.use(express.static('public'));
+app.use(multipart({
+    uploadDir: path.join(__dirname, 'uploads')
+}));
+
+app.post('/project-files', function(req, res) {
+    'use strict';
+    console.log(req.files.appDataZip);
+    res.send('ok').end();
+});
 
 var port = 3300;
 
